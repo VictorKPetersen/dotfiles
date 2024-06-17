424da6bfd05b0ec48d4fc9f718c6554366e363b8
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2024-06-17 05:21:29 +0200

    Updated grep interface

--- .config/nvim/lua/terra/plugins/telescope.lua
+++ .config/nvim/lua/terra/plugins/telescope.lua
@@ -11,9 +11,6 @@ return {
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-	    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
     end
 }
-
