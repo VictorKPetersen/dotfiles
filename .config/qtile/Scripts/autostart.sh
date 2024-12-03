9e81a8fac17b33cf19f5b7e961606d853d7cdada
VictorKPetersen <victorkahlpetersen@gmail.com>
2024-12-03 13:38:22 +0100

    Removed flameshot

--- .config/qtile/Scripts/autostart.sh
+++ .config/qtile/Scripts/autostart.sh
@@ -2,7 +2,6 @@
 #-- Autostart Programs --#
 nm-applet & # nm-applet systray network applet
 picom & # Launch picom compositor
-flameshot & # Launch flameshot screenshot applet
 
 #-- Set Wallpaper --#
 nitrogen --restore &
---
+++ .config/qtile/options.py
@@ -0,0 +1,9 @@
+import colors
+
+colorScheme = colors.Custom
+
+displayBattery = True
+netInterface = "wlp2s0"
+modKey = "mod4"
+terminalCommand = "alacritty"
+browserCommand = "brave"
