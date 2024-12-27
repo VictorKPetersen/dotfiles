4affa8f3e8e52b4c19b5479ba82866ffb9a80408
VictorKPetersen <victorkahlpetersen@gmail.com>
2024-12-28 00:54:03 +0100

    Addded yank/paste to system

--- .config/nvim/lua/terra/core/base.lua
+++ .config/nvim/lua/terra/core/base.lua
@@ -15,8 +15,8 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 
---Dont display Long lines on multiple lines
-vim.o.wrap = false 
+-- Dont display Long lines on multiple lines
+vim.o.wrap = false
 
 -- Case Insensetive Searching Unless /C or capital in search
 vim.o.ignorecase = true
--- .config/nvim/lua/terra/core/remaps.lua
+++ .config/nvim/lua/terra/core/remaps.lua
@@ -4,3 +4,9 @@ vim.g.localmapleader = " "
 
 vim.keymap.set("n", "<leader>tt", "<cmd>Explore<cr>")
 
+-- Remap <leader>y/p to yank and paste from system clipboard
+vim.keymap.set("n", "<leader>y", [["+y]])
+vim.keymap.set("v", "<leader>y", [["+y]])
+
+vim.keymap.set("n", "<leader>p", [["+p]])
+vim.keymap.set("v", "<leader>p", [["+p]])
