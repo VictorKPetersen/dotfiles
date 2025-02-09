398a5e6b68297b838144efbfb2c9dc59c5d376ce
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-02-09 03:27:19 +0100

    Added onedark.nvim colorscheme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+    "navarasu/onedark.nvim",
+    opts = {
+        style = "deep", -- Default style. Choose between "dark", "darker", "cool", "deep", "warm", "warmer", "light"
+        transparent = true,
+        term_colors = true, -- Change terminal colors to selected theme
+
+        -- Options are italic, bold, underline, none
+        -- Configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+        code_style = {
+            comments = 'italic',
+            keywords = 'bold',
+            functions = 'italic,bold',
+            strings = 'none',
+            variables = 'none'
+        },
+    },
+
+    config = function(_, opts)
+        require("onedark").setup(opts)
+        require("onedark").load()
+    end,
+}
