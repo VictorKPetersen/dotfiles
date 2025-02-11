9f3ecf7c7a1e5dcdc858156b5a85bf5872b4d550
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-02-11 03:12:42 +0100

    Added mini.files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 vim.g.mapleader = " "
 vim.g.localmapleader = " "
 
-vim.keymap.set("n", "<leader>tt", "<cmd>Explore<cr>", { desc = "Open file explorer" })
-
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste system clipboard after cursor" })
 vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]], { desc = "Paste system clipboard before cursor" })
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -6,4 +6,39 @@ return {
             style = "glyph",
         },
     },
+
+    {
+        "echasnovski/mini.comment",
+
+        opts = {},
+    },
+
+    {
+        "echasnovski/mini.files",
+
+        keys = {
+            {
+                "<leader>fm",
+                function ()
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                end,
+                desc = "Open mini.files manager"
+            },
+        },
+
+        opts = {
+            options = {
+                use_as_default_explorer = true,
+            },
+
+            windows = {
+                preview = true,
+                width_focus = 35,
+                width_nofocus = 30,
+                width_preview = 30,
+            },
+        },
+
+
+    },
 }
