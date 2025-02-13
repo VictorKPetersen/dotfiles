b588b1547216d4a0be369c74634dca825ceb30b3
VictorKPetersen <victorkahlpetersen@gmail.com>
2025-02-13 10:43:16 +0100

    Disable ruff hover

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -70,6 +70,22 @@ return {
                 { desc = "Jumps to the definition of the symbol under the cursor" })
             vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action,
                 { desc = "Code action for the symbol under the cursor" })
+
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                -- Dsiable ruff hover functionallity
+                group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if client == nil then
+                        return
+                    end
+                    if client.name == 'ruff' then
+                        client.server_capabilities.hoverProvider = false
+                    end
+                end,
+                desc = 'LSP: Disable hover capability from Ruff',
+            })
         end,
 
     }
