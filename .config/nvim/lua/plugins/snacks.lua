18bda479c150a3f08a2ccfb20fe45904854f14a9
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-02-14 13:36:49 +0100

    Snacks Dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,7 +1,6 @@
 return {
     "folke/snacks.nvim",
-
-    event = "VeryLazy",
+    lazy = false, -- Can't lazyload due to using the dashboard
 
     opts = {
         input = {
@@ -37,6 +36,31 @@ return {
                 },
             },
         },
+
+        dashboard = {
+            enabled = true,
+
+            preset = {
+                keys = {
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    -- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                    -- { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+
+            sections = {
+                { section = "header", },
+                { section = "keys", gap = 1, padding = 1, },
+                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+            },
+        },
+
     },
 
     keys = {
