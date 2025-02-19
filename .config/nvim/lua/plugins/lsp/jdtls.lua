582f04251d65057a068b7bca8311f43d17bea3b8
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-02-19 22:48:55 +0100

    autocmd for attatching jdtls

--- .config/nvim/lua/plugins/lsp/jdtls.lua
+++ .config/nvim/lua/plugins/lsp/jdtls.lua
@@ -11,5 +11,13 @@ return {
         -- This starts a new client & server,
         -- or attaches to an existing client & server depending on the `root_dir`.
         require("jdtls").start_or_attach(config)
+
+        -- Attatch to each java buffer
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "java" },
+            callback = function()
+                require("jdtls").start_or_attach(config)
+            end,
+        })
     end,
 }
