2e5f505f4786a7495cbe5829a590111941246533
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-03-05 21:53:17 +0100

    Qtile for semester project

--- .config/qtile/keymaps.py
+++ .config/qtile/keymaps.py
@@ -81,7 +81,7 @@ def initMouseMaps():
     mouseMaps = [
         # Drag floating layouts.
         Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-        Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+        Drag([mod, "shift"], "Button1", lazy.window.set_size_floating(), start=lazy.window.get_size()),
         Click([mod], "Button2", lazy.window.bring_to_front()),
     ]
 
---
+++ .config/qtile/options.py
@@ -0,0 +1,11 @@
+import colors
+
+colorScheme = colors.Custom
+
+displayBattery = False
+netInterface = "eno1"
+modKey = "mod4"
+terminalCommand = "alacritty"
+browserCommand = "brave"
+runLauncher = "rofi -show drun"
+fileManagerCommand = "alacritty -e sh -c ranger"
