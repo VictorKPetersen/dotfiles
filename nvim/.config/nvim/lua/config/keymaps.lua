a656f6df75fa562d8ccc2a1732c27e986f0462b3
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-10-23 10:02:30 +0200

    Add keymaps for diagnostic handeling

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,18 @@ vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]], { desc = "Paste system clipbo
 vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]], { desc = "Paste system clipboard before cursor" })
 
 vim.keymap.set("n", "<leader><cr>", ":nohlsearch<cr>", { desc = "Clear search highlight" })
+
+vim.keymap.set("n", "<leader>do", function ()
+    vim.diagnostic.open_float()
+end, { desc = "Open diagnostic window" }
+)
+
+vim.keymap.set("n", "<leader>dn", function ()
+    vim.diagnostic.jump({ count = 1, float = true })
+end, { desc = "Jump to next diagnostic window" }
+)
+
+vim.keymap.set("n", "<leader>dp", function ()
+    vim.diagnostic.jump({ count = -1, float = true })
+end, { desc = "Jump to prev diagnostic window" }
+)
