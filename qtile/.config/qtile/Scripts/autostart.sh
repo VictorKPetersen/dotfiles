d1e894cb59533154350f77ae6a25aafb1605b8af
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-10-24 10:54:13 +0200

    Set qtile to use gtk portal

--- qtile/.config/qtile/Scripts/autostart.sh
+++ qtile/.config/qtile/Scripts/autostart.sh
@@ -7,3 +7,6 @@ nitrogen --restore &
 nm-applet & # nm-applet systray network applet
 picom & # Launch picom compositor
 dunst & # Launch dunst notification daemon
+
+#-- Start desktop-portal manually --#
+/usr/lib/xdg-desktop-portal-gtk &
