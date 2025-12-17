ce24f87a1d0675c6e346563a62dc76bb35a7d4c6
Victor Kahl Petersen <victorkahlpetersen@gmail.com>
2025-12-18 00:42:30 +0100

    Update treesitter to track main

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,19 @@
 return {
     "nvim-treesitter/nvim-treesitter",
 
-    build = ":TSUpdate",
     lazy = false,
-    event = { "BufNewFile", "BufReadPost", "BufWritePost", },
+    build = ":TSUpdate",
+    branch = "main",
 
     opts = {
         highlight = { enable = true },
         indent = { enable = true },
-        ensure_installed = {
+    },
+
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        ts.install({
             "bash",
             "c",
             "cpp",
@@ -23,10 +28,35 @@ return {
             "python",
             "vim",
             "vimdoc",
-        },
-    },
+        })
 
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
+        local group = vim.api.nvim_create_augroup("TreeSitterSetup", { clear = true })
+
+        local ignore_filetypes = {
+            "checkhealth",
+            "lazy",
+            "mason",
+            "snacks_dashboard",
+            "snacks_notif",
+        }
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = group,
+            desc = "Enable treesitter highlighting and indentation",
+            callback = function(event)
+                if vim.tbl_contains(ignore_filetypes, event.match) then
+                    return
+                end
+
+                local lang = vim.treesitter.language.get_lang(event.match) or event.match
+                local buf = event.buf
+
+                pcall(vim.treesitter.start, buf, lang)
+
+                vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+                ts.install({ lang })
+            end,
+        })
     end,
 }
